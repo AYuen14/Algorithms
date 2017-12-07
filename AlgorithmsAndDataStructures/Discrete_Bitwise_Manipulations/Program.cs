a85121a0af911e3e87d254e469abd819cf654379
CAMPUS\adam.yuen <adamyuen07@yahoo.com>
2017-12-06 19:28:26 -0800

    Added bit minipulation

---
+++ AlgorithmsAndDataStructures/Discrete_Bitwise_Manipulations/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discrete_Bitwise_Manipulations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int selection;
+            do
+            {
+                DisplayMenu();
+                Console.Write("\nEnter your selection: ");
+                int.TryParse(Console.ReadLine(), out selection);
+                switch (selection)
+                {
+                    case 1:
+                        PerformAndOperation();
+                        break;
+                    case 2:
+                        PerformORoperation();
+                        break;
+                    case 3:
+                        PerformXORoperation();
+                        break;
+                    case 4:
+                        PerfromNOToperation();
+                        break;
+                    case 5:
+                        ShiftRight();
+                        break;
+                    case 6:
+                        break;
+                    case 7:                       
+
+                        break;
+                    case 8:
+                        uint n,position;
+                        
+                        Console.Write("Enter a positive integer: ");
+                        uint.TryParse(Console.ReadLine(), out n);
+                        Console.WriteLine("Its binary representation is: {0}\n", ConvertIntToBinary(n).PadLeft(32, '0'));
+
+                        Console.Write("Enter position to clear: ");
+                        uint.TryParse(Console.ReadLine(), out position);
+
+                        uint result = ClearBit(n, position);
+
+                        break;
+                    default:
+                        break;
+                }//end of switch
+
+                if (selection != 9)
+                {
+                    //clear screen only if not exiting
+                    Console.Write("\nHit any key to continue");
+                    Console.ReadKey();//pause
+                    Console.Clear();
+                }
+
+            } while (selection != 9);
+
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("\n==========MENU=========================================\n");
+            Console.WriteLine("\t1. AND Operation ");
+            Console.WriteLine("\t2. OR Operation");
+            Console.WriteLine("\t3. XOR Opertation");
+            Console.WriteLine("\t4. NOT Operation");
+            Console.WriteLine("\t5. SHIFT RIGHT Operation");
+            Console.WriteLine("\t6. SHIFT LEFT Operation");
+            Console.WriteLine("\t7. SET A PARTICULAR BIT IN A WORD");
+            Console.WriteLine("\t8. CLEAR A PARTICULAR BIT IN A WORD");
+            Console.WriteLine("\t9. Exit Application");
+            Console.WriteLine("\n=======================================================");
+        }
+    //Helper method
+        //method to take an int and return its binary representation in string format
+        static string ConvertIntToBinary(uint n)
+        {
+            return Convert.ToString(n, 2); //where n is the integer value to convert and 2 is the base (for binary)
+        }
+
+        //method to convert a binary in string format to int
+        static uint ConvertBinToInt(string binary)
+        {
+            return Convert.ToUInt32(binary, 2);//convert the string binary from base 2 to int
+        }
+       
+        //============================CASES===========================
+        //case 1:
+        static void PerformAndOperation()
+        {
+            uint n1, n2;
+            Console.Write("Enter the first integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n1);
+            Console.Write("Enter the second integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n2);
+
+            //Apply the AND operation
+            uint result = n1 & n2;
+            //display 
+            Console.WriteLine("\n\nAND OPERATION:\n");
+            Console.WriteLine("{0} AND {1} = {2}", n1, n2, result);
+
+            Console.WriteLine("\nIn binary form:\n");
+            Console.WriteLine("{0}    AND \n{1} \n= \n{2}",
+                ConvertIntToBinary(n1).PadLeft(32, '0'),
+                ConvertIntToBinary(n2).PadLeft(32, '0'), 
+                ConvertIntToBinary(result).PadLeft(32, '0'));
+
+
+        }
+        //------------------------------------------------------------------------------
+        //Case 2: OR operation
+        static void PerformORoperation()
+        {
+            uint n1, n2;
+            Console.Write("Enter the first integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n1);
+            Console.Write("Enter the second integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n2);
+
+            //Apply the OR operation
+            uint result = n1 | n2;
+            //display 
+            Console.WriteLine("\n\nOR OPERATION:\n");
+            Console.WriteLine("{0} OR {1} = {2}", n1, n2, result);
+
+            Console.WriteLine("\nIn binary form:\n");
+            Console.WriteLine("{0}    OR \n{1} \n= \n{2}",
+                ConvertIntToBinary(n1).PadLeft(32, '0'),
+                ConvertIntToBinary(n2).PadLeft(32, '0'),
+                ConvertIntToBinary(result).PadLeft(32, '0'));
+
+        }
+        //------------------------------------------------------------
+        //Case 3: XOR operation
+        static void PerformXORoperation()
+        {
+            uint n1, n2;
+            Console.Write("Enter the first integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n1);
+            Console.Write("Enter the second integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n2);
+
+            //Apply the XOR operation
+            uint result = n1 ^ n2;
+            //display 
+            Console.WriteLine("\n\nXOR OPERATION:\n");
+            Console.WriteLine("{0} XOR {1} = {2}", n1, n2, result);
+
+            Console.WriteLine("\nIn binary form:\n");
+            Console.WriteLine("{0}    XOR \n{1} \n= \n{2}",
+                ConvertIntToBinary(n1).PadLeft(32, '0'),
+                ConvertIntToBinary(n2).PadLeft(32, '0'),
+                ConvertIntToBinary(result).PadLeft(32, '0'));
+        }
+        //Case 4: logical Not operation
+        static void PerfromNOToperation()
+        {
+            uint n;
+            Console.Write("Enter the first integer value from: {0} to {1}  : ", uint.MinValue, uint.MaxValue);
+            uint.TryParse(Console.ReadLine(), out n);
+            ///There is no such operation, but it could be
+            ///accomplished using an XOR operation
+            ///between the value n and some value, called mask
+            uint mask = 0xFFFFFFFF;
+            //or
+            //int mask = ConvertBinToInt("11111111111111111111111111111111");
+           uint result = n ^ mask;
+            //display n
+            //display result: n after inverting
+           Console.WriteLine("{0}\n Not   \n{1} ",
+              ConvertIntToBinary(n).PadLeft(32, '0'),
+              ConvertIntToBinary(result).PadLeft(32, '0'));
+
+        }
+        
+        //case 5: shift right
+        static void ShiftRight()
+        {
+            uint n;
+            int count;
+            Console.Write("Enter  integer value to shift right  : ");
+            uint.TryParse(Console.ReadLine(), out n);
+            Console.Write("Enter number of times to shift right.  count should be in the range: 1 - 32  : ");
+            int.TryParse(Console.ReadLine(), out count);
+
+            Console.WriteLine("\n\nBefore right shifting:  \n");
+            Console.WriteLine(ConvertIntToBinary(n).PadLeft(32,'0'));
+            uint result = n >> count;
+            Console.WriteLine("\nAfter right shifting: \n");
+            Console.WriteLine(ConvertIntToBinary(result).PadLeft(32,'0'));
+
+        }
+        //case 8:Clear a bit. Let position be zero-based
+        static uint ClearBit(uint n, uint position)
+        {
+            /// to clear a specific bit in a binary number n, you must
+            /// force it to be zero. That is accomplished by ANDing it
+            /// with a bit=0. Since you don't want the rest of the bits
+            /// in n to change, you must AND them with 1's
+            /// 
+            ///Basically you want to AND the value n with a mask value
+            ///where all the bits are 1 except for the designated bit
+            ///intended to cause a reset
+            ///
+            ///Example: Given a binary number 11001010
+            ///recall the right most bit is bit0 (LSB)
+            ///Let's say you want to clear bit3 (set it to 0) while
+            ///leaving the rest of the bits unchanged. then you need
+            ///to come up with a mask value = 11110111
+            ///Then when you AND n with mask, the result is n with just
+            ///bit3 cleared.
+            ///
+            ///The question is how to generate a mask, once you know
+            ///what bit to clear
+            ///The weight value at bit3 is 8 (2 to the 3)
+            ///The Max value the mask can have is when all the bits are 1
+            ///Take the Max value and subtract the effect of the 3rd bit
+            ///
+            uint bitweight = (uint) Math.Pow(2, position); //all the bits are zero except bit at the specified position
+            //inverse the mask, so that all the bits are 1 except the bit at the position
+            uint mask = 0xFFFFFFFF - bitweight;//the mask has all the bits on except bit at position
+
+            uint result = n & mask;
+
+            Console.WriteLine("n: \n\t {0} \nmask: \n\t {1} \nresult: \n\t {2}",
+                ConvertIntToBinary(n).PadLeft(32, '0'), ConvertIntToBinary(mask).PadLeft(32, '0'), ConvertIntToBinary(result).PadLeft(32, '0'));
+
+            return result;
+
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/The_BitArray_Collection/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace The_BitArray_Collection
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Constructors
+            //============
+
+          //  //1. //create a BitArray from an array of boolean values
+          //  bool[] barray = { true, false, false, true, true, true, false, false,true,true,false,true };
+          //  BitArray ba1 = new BitArray(barray);
+          //  DisplayBitArray(ba1);
+          //  DisplayBitArrayMostToLeast(ba1);
+
+          //  //2: create a BitArray from bytes
+          ////  byte[] bytes = { 0xE1, 0x3D, 0x5E, 0x9A };
+          //  byte[] bytes = { 1, 2, 3, 4 };
+          //  BitArray ba2 = new BitArray(bytes);
+          //  ///The first byte in the array represents bits 0 through 7, the second byte represents bits 8 through 15, and so on. 
+          //  ///The Least Significant Bit of each byte represents the lowest index value: " bytes [0] & 1" represents bit 0, 
+          //  ///" bytes [0] & 2" represents bit 1, " bytes [0] & 4" represents bit 2, and so on.
+          //  DisplayBitArray(ba2);
+          //  DisplayBitArrayMostToLeast(ba2);
+
+          //  //3: Create a BitArray that holds 32 bits
+          //  BitArray ba3 = new BitArray(32); //initially all the bits are set to false
+          //  DisplayBitArray(ba3);
+          //  DisplayBitArrayMostToLeast(ba3);
+
+          //  //4. Create a BitArray from an array of ints
+          //  BitArray ba4 = new BitArray(new int[] {1,2 });
+          //  ///The number in the first values array element represents bits 0 through 31, 
+          //  ///the second number in the array represents bits 32 through 63, and so on. 
+          //  ///The Least Significant Bit of each integer represents the lowest index value: 
+          //  ///" values [0] & 1" represents bit 0, " values [0] & 2" represents bit 1, " values [0] & 4" represents bit 2, and so on.
+          //  DisplayBitArray(ba4);
+          //  DisplayBitArrayMostToLeast(ba4);
+
+          //  //5. Create a BitArray that holds 32 bits, all set to true (1)
+          //  BitArray ba5 = new BitArray(32, true);
+          //  DisplayBitArray(ba5);
+          //  DisplayBitArrayMostToLeast(ba5);
+
+            //Methods
+            //=======
+            BitArray ba6 = new BitArray(8); //bit array holding 8 bits initially set to false
+            Console.WriteLine("\n\nBit array of size 8, initially all bits are set to false");
+            DisplayBitArray2(ba6);
+
+            
+            //Set the bit at index 1 to true
+            Console.WriteLine("\n\nSet Bit 1 and bit count-2 to true:\n");
+            ba6.Set(1, true);
+            //set bit at index count-2 to true
+            ba6.Set(ba6.Count - 2, true);
+            DisplayBitArray2(ba6);
+
+            //invert it
+            Console.WriteLine("\n\nInvert all the bits: \n");
+            ba6.Not();
+            DisplayBitArray2(ba6);
+
+            ///=========================================================================================
+            //toggle all the bits
+            //use the Xor method
+            BitArray toggler = new BitArray(new byte[] { 255 });
+            ba6.Xor(toggler);
+            DisplayBitArray2(ba6);
+
+            //toggle bit2 and bit5
+            BitArray toggler2 = new BitArray(new bool[] { false, false, true, false, false, true, false, false });
+            ba6.Xor(toggler2);
+            DisplayBitArray2(ba6);
+
+            //pause
+            Console.ReadLine();
+        }
+
+        static void DisplayBitArray(BitArray ba)
+        {
+            Console.WriteLine("\n\n");
+            int count = 0;
+            foreach(bool b in ba)
+            {
+                count++;
+                // Convert bool to int
+                int i = b ? 1 : 0;
+                Console.Write("{0} ", i);
+
+                if (count % 8 == 0)
+                    Console.Write(" ");
+
+                   
+            }
+        }
+        static void DisplayBitArrayMostToLeast(BitArray ba)
+        {
+            Console.WriteLine("\n");
+            int count = 0;
+            for (int i = ba.Count-1; i >= 0; i--)
+            {
+                bool b = ba[i];
+
+                count++;
+                // Convert bool to int
+                int n = b ? 1 : 0;
+                Console.Write("{0} ", n);
+
+                if (count % 8 == 0)
+                    Console.Write(" ");
+
+            }
+           
+        }
+        static void DisplayBitArray2(BitArray ba)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < ba.Count; i++)
+            {
+                bool b = ba[i];
+                Console.WriteLine("[{0}] = {1}",i,b);
+            }
+        }
+    }
+}
